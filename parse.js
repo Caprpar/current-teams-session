edea8a64b5eeb3d34c66e6a09826abf4275b416e
Caspar Danielsson <caspar.danielsson@iths.se>
2025-02-04 13:59:15 +0100

    add agile links to tuesdays

--- parse.js
+++ parse.js
@@ -4,24 +4,16 @@ const today = thisDay.toISOString().split("T")[0];
 let hrefElement = document.querySelector("a");
 let dateElement = document.querySelector("#date > h1");
 document.querySelector("#dateEl").textContent = today;
-const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let weekday = weekdays[thisDay.getDate()];
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let weekday = weekdays[thisDay.getDay()];
 
 fetch("api.json")
   .then((response) => response.json())
   .then((result) => {
     for (const day of result) {
-      if (day.date === today || day.date === weekdays[thisDay.getDate()]) {
+      if (day.date === today || day.date === weekday) {
         hrefElement.href = day.link;
-        dateElement.textContent = day.date;
+        dateElement.textContent = today;
         console.log(day.date);
         console.log(weekday);
         if (!day.isRemote) {
